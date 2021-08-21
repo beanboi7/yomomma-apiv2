33dce5e9cbe093e3dad7cfe695e9bda66c6d8326
Pranjal Kole <pranjal.kole7@gmail.com>
2021-08-21 18:55:25 +0530

    A few minor changes
    Replace import of random with just import of random.choice
    Remove parameters for home func since it served no purpose (afaik)
    Replace length variable with len function since it is O(1) in Python
    Check the entirety of joke in search_joke

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import random
+from random import choice
 from typing import Optional
 from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
@@ -14,7 +14,6 @@ from slowapi.middleware import SlowAPIMiddleware
 
 with open('./jokes.json') as f:
     all_jokes = json.load(f)
-no_of_jokes = len(all_jokes)
 
 limiter = Limiter(key_func=get_remote_address, default_limits = ["5/minute"])
 app = FastAPI()
@@ -23,31 +22,31 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
 
 @app.get("/")
-async def home(request: Request):
+async def home():
     return {"joke": "Yo momma"}
 
 
 @app.get("/jokes")
 async def send_joke(count: Optional[int] = 1):
-    if count > 1 and count < no_of_jokes:
-        return [{"joke": random.choice(all_jokes)} for i in range(count)]
+    if count > 1 and count < len(all_jokes):
+        return [{"joke": choice(all_jokes)} for i in range(count)]
     elif count == 1:
-        return {"joke": random.choice(all_jokes)}
+        return {"joke": choice(all_jokes)}
     else:
         raise HTTPException(status_code=404, detail="Invalid count parameter")
 
 
 @app.get("/jokes/{index}")
 async def send_specific_joke(index: int):
-    if index > no_of_jokes or index < 0:
+    if index > len(all_jokes) or index < 0:
         raise HTTPException(status_code=404, detail="Index out of range")
     return {"joke": all_jokes[index]}
 
 
 @app.get("/search")
 async def search_joke(query: str):
-    if query == "Yo momma" or query == "yo momma":
+    if query.lower() == "yo momma":
         return {"results": "DON'T"}
 
-    result = [joke for joke in all_jokes if query in joke[:-1].lower().split()]
+    result = [joke for joke in all_jokes if query in joke.lower().split()]
     return {"results": result}
