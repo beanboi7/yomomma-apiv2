7b3330185b1048239ed971d30a2864970e33b885
Vaibhav Jaiswal <vaibhav.09047@gmail.com>
2022-10-24 12:02:02 +0530

    Fixed cors origin issue

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 with open('./jokes.json') as f:
     all_jokes = load(f)
@@ -20,7 +21,12 @@ app = FastAPI()
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def home():
     return {"joke": "Yo momma"}
